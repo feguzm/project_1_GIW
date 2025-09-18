c9cf2f3076edb699c800b0af68d05f7663d238f3
PabloBerucm <115647235+PabloBerucm@users.noreply.github.com>
2025-09-18 17:04:07 +0200

    Add files via upload

---
+++ pr1_skel.py
@@ -0,0 +1,45 @@
+"""
+TODO: rellenar
+
+Asignatura: GIW
+Práctica 1
+Grupo: XXXXXXX
+Autores: XXXXXX 
+
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
+sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
+de fuentes externas, y tampoco hemos compartido nuestra solución con otras personas
+de manera directa o indirecta. Declaramos además que no hemos realizado de manera
+deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
+resultados de los demás.
+"""
+
+
+# Ejercicio 1
+
+def dimension(matriz):
+    ...
+
+def es_cuadrada(matriz):
+    ...
+
+def es_simetrica(matriz):
+    ...
+
+def multiplica_escalar(matriz, k):
+    ...
+
+def suma(matriz1, matriz2):
+    ...
+
+
+# Ejercicio 2
+def validar(grafo):
+    ...
+
+def grado_entrada(grafo, nodo):
+    ...
+
+def distancia(grafo, nodo):
+    ...
+   
