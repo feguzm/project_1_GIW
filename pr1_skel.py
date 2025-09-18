e08d25bcb9f2e79da68c9d5678d0ec7a234b5b8a
PabloBerucm <115647235+PabloBerucm@users.noreply.github.com>
2025-09-18 17:20:48 +0200

    Update pr1_skel.py

--- pr1_skel.py
+++ pr1_skel.py
@@ -18,13 +18,20 @@ resultados de los demás.
 # Ejercicio 1
 
 def dimension(matriz):
-    ...
+    filas = 0
+    columnas = 0
+    for x in matriz:
+        filas += 1
+        for y in matriz[x]:
+            columnas += 1
+    return (filas, columnas)
 
 def es_cuadrada(matriz):
     ...
 
 def es_simetrica(matriz):
     ...
+
 
 def multiplica_escalar(matriz, k):
     ...
@@ -43,3 +50,14 @@ def grado_entrada(grafo, nodo):
 def distancia(grafo, nodo):
     ...
    
+
+#a partir de aquí está el código para probar las funciones
+matriz1 = [[1, 0, 2, 5], 
+           [0, 3, 3, 5], 
+           [1, 2, 2, 5]]
+
+matriz2 = [[1, 0, 1], 
+           [0, 3, 2], 
+           [1, 2, 2]]
+
+print(dimension(matriz1))
